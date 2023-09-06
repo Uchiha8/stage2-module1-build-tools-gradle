45f041b22998e2e7fca61daa569d987b617794a6
AliNajimov <najimovm11@gmail.com>
2023-09-06 13:34:33 +0500

    .

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,8 +2,20 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         int val = Integer.parseInt(str);
-         if(val>=0) return true;
-         return false;
+        if (str == null) {
+            return false;
+        }
+        try {
+            int num = Integer.parseInt(str);
+            int firstLetter =  Integer.parseInt(str.substring(0, 1));
+            return num > 0 && firstLetter > 0;
+        } catch (NumberFormatException e) {
+            try {
+                double num = Double.parseDouble(str);
+                return num > 0;
+            } catch (NumberFormatException ex) {
+                return false;
+            }
+        }
     }
 }
